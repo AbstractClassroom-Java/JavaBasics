2876bfd1b20083bd9e71e32cfbec9f1c31332d10
Nathan Russell <nathan@blackpathway.com>
2026-01-16 11:15:22 -0500

    initial commit

---
+++ JavaBasics/src/main/java/io/github/nathanjrussell/Main.java
@@ -0,0 +1,19 @@
+package io.github.nathanjrussell;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("While Count: " + i);
+            i++;
+        }
+    }
+}
