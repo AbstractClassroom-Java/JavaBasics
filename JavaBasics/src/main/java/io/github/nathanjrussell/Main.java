45a81bca7c5827e9b685dcdec0b87b993be5ff62
Marcos Duenas <mduenas534@gmail.com>
2026-01-23 12:39:35 -0500

    yo add this

--- JavaBasics/src/main/java/io/github/nathanjrussell/Main.java
+++ JavaBasics/src/main/java/io/github/nathanjrussell/Main.java
@@ -4,16 +4,10 @@ package io.github.nathanjrussell;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Everyone!");
+        Cooler mySwapCoolerClass = new Cooler();
+        System.out.println(mySwapCoolerClass.getBirthYear());
+        mySwapCoolerClass.setBirthYear(2007);
+        System.out.println(mySwapCoolerClass.getBirthYear());
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Count: " + i);
-        }
-
-        int i = 1;
-        while (i <= 5) {
-            System.out.println("While Count: " + i);
-            i++;
-        }
     }
 }
